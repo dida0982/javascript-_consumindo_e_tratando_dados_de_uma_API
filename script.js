a40f78c4470992809f70d90e0e7c98e9c2e6ea30
Guilherme Barros JR <dida0982@gmail.com>
2024-10-28 16:30:46 -0300

    feat(script): Tratar erros com try…catch
    tratar erros em nosso código JavaScript usando o try...catch. O try tenta executar um bloco de código, e se houver um erro, o catch o captura e executa um código específico para lidar com ele.
    
    Também vimos o finally, que é executado independentemente de haver um erro ou não.
    
    Por fim, aprendemos a criar erros personalizados com throw new Error(), que nos permite detectar e tratar erros específicos da nossa aplicação.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const containerVideos = document.querySelector(".videos__container");
 
 
-async function buscarEMostrarVideos() {    
-    const busca = await fetch("http://localhost:3000/videos");
-    const videos = await busca.json();
-
+async function buscarEMostrarVideos() { 
+    try{
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+        
         videos.forEach((video) => {
+            if (video.categoria  == "") {
+                throw new Error("Video nao tem categoria");
+            }
             containerVideos.innerHTML += `
             <li class='videos__item'> 
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe> 
@@ -15,7 +19,10 @@ async function buscarEMostrarVideos() {
             <p class="titulo-canal">${video.descricao}</p>
             </div>
             </li>`;
-    })
+        })
+    } catch(error){
+        containerVideos.innerHTML = `<p> Houve um erro ao validar os videos: ${error} </p>`
+    }
 }
 
 buscarEMostrarVideos();
