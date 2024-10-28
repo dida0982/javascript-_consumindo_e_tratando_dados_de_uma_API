b47c48a9f7ce818b26cbf31f38e05fa2c406a8b1
Guilherme Barros JR <dida0982@gmail.com>
2024-10-28 16:48:50 -0300

    feat(script): Construí a lógica da barra de pesquisa.
    implementar a funcionalidade de pesquisa na nossa aplicação.
    
    Primeiro, criamos um evento que é disparado toda vez que o usuário digita algo na barra de pesquisa.
    
    Depois, dentro da função que trata esse evento, percorremos todos os vídeos da página e comparamos o título de cada vídeo com o texto digitado na barra de pesquisa.
    
    Se o título do vídeo contiver o texto da pesquisa, o vídeo é exibido. Caso contrário, o vídeo é ocultado.
    
    Para finalizar, garantimos que todos os vídeos sejam exibidos caso a barra de pesquisa esteja vazia.
    
    Com isso, nossa barra de pesquisa está funcionando perfeitamente!

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const containerVideos = document.querySelector(".videos__container");
 
+async function buscarEMostrarVideos() {
+  try {
+    const busca = await fetch("http://localhost:3000/videos");
+    const videos = await busca.json();
 
-async function buscarEMostrarVideos() { 
-    try{
-        const busca = await fetch("http://localhost:3000/videos");
-        const videos = await busca.json();
-        
-        videos.forEach((video) => {
-            if (video.categoria  == "") {
-                throw new Error("Video nao tem categoria");
-            }
-            containerVideos.innerHTML += `
+    videos.forEach((video) => {
+      if (video.categoria == "") {
+        throw new Error("Video nao tem categoria");
+      }
+      containerVideos.innerHTML += `
             <li class='videos__item'> 
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe> 
             <div class="descricao-video">
@@ -19,23 +18,34 @@ async function buscarEMostrarVideos() {
             <p class="titulo-canal">${video.descricao}</p>
             </div>
             </li>`;
-        })
-    } catch(error){
-        containerVideos.innerHTML = `<p> Houve um erro ao validar os videos: ${error} </p>`
-    }
+    });
+  } catch (error) {
+    containerVideos.innerHTML = `<p> Houve um erro ao validar os videos: ${error} </p>`;
+  }
 }
 
 buscarEMostrarVideos();
 
-const barraDePesquisa = document.querySelector("pesquisar__input");
+const barraDePesquisa = document.querySelector(".pesquisar__input");
 
 barraDePesquisa.addEventListener("input", filtrarPesquisa);
 function filtrarPesquisa() {
-    const videos = document.querySelectorAll('.videos__item');
+  const videos = document.querySelectorAll(".videos__item");
 
-    if(barraDePesquisa.value != ""){
-        
-    }else{
+  if (barraDePesquisa.value != "") {
+    for (let video of videos) {
+      let titulo = video
+        .querySelector(".titulo-video")
+        .textContent.toLocaleLowerCase();
+      let valorFiltro = barraDePesquisa.value.toLocaleLowerCase();
 
+      if (!titulo.includes(valorFiltro)) {
+        video.style.display = "none";
+      } else {
+        video.style.display = "block";
+      }
     }
+  } else {
+    video.style.display = "block";
+  }
 }
